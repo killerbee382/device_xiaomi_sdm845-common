8fd781b0c459f32c63194d7b0ae0277710c55be7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-01-05 21:46:40 +0000

    sdm845-common: releasetools: Fix incremental ota gen

--- releasetools.py
+++ releasetools.py
@@ -17,37 +17,45 @@
 import re
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, False)
   return
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+  OTA_InstallEnd(info, True)
   return
 
 def FullOTA_Assertions(info):
-  AddModemAssertion(info, info.input_zip)
+  AddModemAssertion(info, False)
   return
 
 def IncrementalOTA_Assertions(info):
-  AddModemAssertion(info, info.target_zip)
+  AddModemAssertion(info, True)
   return
 
-def AddImage(info, basename, dest):
+def AddImage(info, basename, dest, incremental):
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
   path = "IMAGES/" + basename
-  if path not in info.input_zip.namelist():
+  if path not in input_zip.namelist():
     return
 
-  data = info.input_zip.read(path)
+  data = input_zip.read(path)
   common.ZipWriteStr(info.output_zip, basename, data)
   info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
 
-def OTA_InstallEnd(info):
-  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
+def OTA_InstallEnd(info, incremental):
+  AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo", incremental)
   return
 
-def AddModemAssertion(info, input_zip):
-  android_info = info.input_zip.read("OTA/android-info.txt")
+def AddModemAssertion(info, incremental):
+  if incremental:
+    input_zip = info.source_zip
+  else:
+    input_zip = info.input_zip
+  android_info = input_zip.read("OTA/android-info.txt")
   m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
   miui_version = re.search(r'require\s+version-miui\s*=\s*(.+)', android_info)
   if m and miui_version:
